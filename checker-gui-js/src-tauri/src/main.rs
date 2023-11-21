0857ea54b1ff88e3b4432518cf78a2511ec331c5
huccik <hucci.royce@gmail.com>
2023-11-22 02:02:06 +0300

    sidecar fix

--- checker-gui-js/src-tauri/src/main.rs
+++ checker-gui-js/src-tauri/src/main.rs
@@ -1,22 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::env;
-use tauri::api::process::{Command, CommandEvent};
-
 
 fn main() {
-    let mut bin = "./bin/cmd-x86_64-pc-windows-msvc.exe";
-    if env::consts::OS == "macos" {
-       bin = "./bin/cmd-aarch64-apple-darwin";
-    }
-
-    println!("{}", bin);
-    tauri::async_runtime::spawn(async move {
-      let (mut rx, mut child) = Command::new(bin)
-        .spawn()
-        .expect("Failed to spawn cargo");
-    });
 
     tauri::Builder::default()
         .plugin(tauri_plugin_websocket::init())
--- checker-gui-js/src/main.jsx
+++ checker-gui-js/src/main.jsx
@@ -5,17 +5,24 @@ import "./styles/styles.css";
 import Store from "./store/store.js";
 import {BrowserRouter} from "react-router-dom";
 import Check from "./store/check.js";
+import {Command} from "@tauri-apps/api/shell";
+const cmd = Command.sidecar("./bin/cmd")
+const output = cmd.execute()
+output.then(r => console.log(r))
 
 const store = new Store();
 const check = new Check();
 
 export const StoreContext = createContext({store, check});
 
+function render() {
+    ReactDOM.createRoot(document.getElementById("root")).render(
+        <BrowserRouter>
+            <StoreContext.Provider value={{store, check}}>
+                <App />
+            </StoreContext.Provider>
+        </BrowserRouter>
+    );
+}
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-        <StoreContext.Provider value={{store, check}}>
-            <App />
-        </StoreContext.Provider>
-    </BrowserRouter>
-);
+setTimeout(render, 1000);
